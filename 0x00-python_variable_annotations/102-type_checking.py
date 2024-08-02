c6d44ac092b96d7ae48bf53d1b03eb12c9366831
vicano-code <vc.anokwuru@gmail.com>
2024-08-02 12:06:44 +0100

    indentation error correction

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,9 +7,10 @@ from typing import Tuple, List, Union
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-  """zoom in on a tuple by repeating each element a specified
-   number of times
-  """
+    """
+    zoom in on a tuple by repeating each element a specified
+    number of times
+    """
     zoomed_in: Tuple = tuple([
       item for item in lst
       for i in range(factor)
