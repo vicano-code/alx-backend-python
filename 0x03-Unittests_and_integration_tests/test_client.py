76fe6b398c5b85ddcb384e666d58e79334b9e5d2
Victor Anokwuru <vc.anokwuru@gmai.com>
2024-08-30 16:45:25 +0000

    integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -65,7 +65,45 @@ class TestGithubOrgClient(unittest.TestCase):
         """Integration test: fixtures
             Integration test for GithubOrgClient.public_repos method
         """
+        """ Tests fixture github org client """
 
+    @classmethod
+    def setUpClass(cls):
+        """ setup before each method"""
+        conf = {'return_value.json.side_effect':
+                [
+                    cls.org_payload, cls.repos_payload,
+                    cls.org_payload, cls.repos_payload
+                ]
+                }
+        cls.get_patcher = patch('requests.get', **conf)
+
+        cls.mock = cls.get_patcher.start()
+
+    def test_public_repos(self):
+        """ More integration test for public repos """
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """ test public repos with license """
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.assertEqual(test_class.public_repos(
+            "apache-2.0"), self.apache2_repos)
+        self.mock.assert_called()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ teardown after of each class """
+        cls.get_patcher.stop()
 
 if __name__ == '__main__':
     unittest.main()
